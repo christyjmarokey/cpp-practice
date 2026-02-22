0aa6773c117ae9659bc85a5842f189089aeeaf76
AEmmi4U <aemmiforyou@gmail.com>
2026-02-22 18:14:54 +0100

    Add square root and exponentiation program

--- calculating_square_root.cpp
+++ calculating_square_root.cpp
@@ -1,3 +1,8 @@
+/* Write a program to calculate the square root of a double number x (received
+as input) with two methods using sqrt(x) and pow(x, 0.5). Print out the two
+results to 2 digits after the decimal point. Remember to check the positivity of
+x before the calculation.*/
+
 #include <iostream>
 #include <math.h>
 #include <iomanip>
